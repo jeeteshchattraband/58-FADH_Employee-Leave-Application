234f2949ac5e7c1d0b3035e649c79b087b7c153e
jeeteshchattraband <jeeteshchattraband@gmail.com>
2019-03-14 01:21:58 +0530

    Employee Leave Application

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from './material/material.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeService } from './shared/employee.service';
+import { environment } from '../environments/environment';
+import { DepartmentService } from './shared/department.service';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { DatePipe } from '@angular/common';
+import { MatConfirmDialogComponent } from './mat-confirm-dialog/mat-confirm-dialog.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent,
+    MatConfirmDialogComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    FormsModule
+  ],
+  providers: [EmployeeService, DepartmentService, DatePipe],
+  bootstrap: [AppComponent],
+  entryComponents: [EmployeeComponent, MatConfirmDialogComponent]
+})
+export class AppModule { }
